847922910e6b0608c3e2738ff7a1870302678329
Erling Owe <owe@vecora.no>
2013-02-07 00:10:56 +0100

    Changed license

--- jquery.slides.js
+++ jquery.slides.js
@@ -2,20 +2,9 @@
  * slides | jQuery plugin
  * Copyright (C) 2013 Vecora AS
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Released under the MIT license and GNU GPLv3
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see http://www.gnu.org/licenses/.
- *
- * @author Erling Owe <owe@vecora.no>
+ * @author Erling Owe <owe@vecora.com>
  * @copyright Vecora AS, 2013
  */
 
